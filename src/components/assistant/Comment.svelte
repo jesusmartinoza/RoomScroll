5e9e18f037f5e4f19e550079545a75aef33da3a1
Jesus Martinez <jesusmartinoza@gmail.com>
2024-07-19 14:09:49 -0600

    Simple layout UI

---
+++ src/components/assistant/Comment.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let avatar = "https://placehold.co/40x40";
+  export let name = "";
+  export let message = "";
+</script>
+
+<div class="bg-white rounded-2xl p-4">
+  <div class="flex items-start space-x-4">
+    <div class="flex-shrink-0">
+      <img class="h-10 w-10 rounded-full" src={avatar} alt="{name} Avatar" />
+    </div>
+    <div class="flex-grow">
+      <p class="text-sm font-medium text-gray-900">{name}</p>
+      <p class="mt-1 text-sm text-gray-500">
+        {message}
+      </p>
+    </div>
+  </div>
+</div>
---
+++ src/components/assistant/Title.svelte
@@ -0,0 +1,15 @@
+<script>
+  export let number;
+  export let text;
+</script>
+
+<div class="p-3 bg-[#EEEEEE] rounded-full">
+  <div class="flex items-center">
+    <div
+      class="w-8 h-8 bg-lime-300 rounded-full flex items-center justify-center text-black font-bold mr-3 text-2xl"
+    >
+      {number}
+    </div>
+    <h2 class="text-lg font-semibold">{text}</h2>
+  </div>
+</div>
