0760c6e7004cd53e3dc4c536454c0f43779c28da
Jesus Martinez <jesusmartinoza@gmail.com>
2024-07-23 19:34:05 -0600

    Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import svelte from "@astrojs/svelte";
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   output: 'server',
-  integrations: [svelte(), tailwind()]
+  integrations: [svelte(), tailwind()],
+  adapter: vercel()
 });
